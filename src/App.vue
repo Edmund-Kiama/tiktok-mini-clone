5aa2ae5a76f147e67099bf8e70262f79fcafebd6
Edmund <edmundgachanja@gmail.com>
2025-01-04 18:35:20 +0300

    Adds videoList and incorporates the array to mainContent component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,9 @@
 <template>
   <headTag></headTag>
-
   <body>
     <topNav></topNav>
     <SideBar></SideBar>
-    <mainContent></mainContent>
+    <mainContent :video-list="videoList" ></mainContent>
 
   </body>  
 </template>
@@ -14,4 +13,43 @@
   import headTag from './components/headTag.vue';
   import SideBar from './components/sideBar.vue';
   import mainContent from './components/mainContent.vue';
+
+  const videoList = [
+    {profilePic: "https://i.pinimg.com/236x/85/7c/40/857c4052f8fd60303bebf8e372bc2f79.jpg" ,
+        video: "https://videos.pexels.com/video-files/29969740/12860951_360_640_30fps.mp4",
+        like: 201,
+        comments: 311,
+        save: 50,
+        shares:203
+    },
+    {profilePic: "https://i.pinimg.com/474x/2b/25/28/2b2528d577b63960e3a30b254b3755c8.jpg" ,
+        video: "https://videos.pexels.com/video-files/28167731/12315786_360_640_25fps.mp4",
+        like: 2876,
+        comments: 389,
+        save: 90,
+        shares:1093
+    },
+    {profilePic: "https://i.pinimg.com/236x/1e/ae/67/1eae674712ab8d73f05256951f4fa098.jpg" ,
+        video: "https://videos.pexels.com/video-files/17844988/17844988-sd_360_640_30fps.mp4",
+        like: 561,
+        comments: 39,
+        save: 489,
+        shares: 233
+    },
+    {profilePic: "https://i.pinimg.com/236x/e8/e6/41/e8e64141f4c0ae39c32f9701ccea9a2e.jpg" ,
+        video: "https://videos.pexels.com/video-files/26256159/11943214_360_640_60fps.mp4",
+        like:571,
+        comments: 321,
+        save: 73,
+        shares:85
+    },
+    {profilePic: "https://i.pinimg.com/236x/6d/3e/b8/6d3eb8147fdcdd571952e667b6690801.jpg",
+        video:  "https://videos.pexels.com/video-files/6547499/6547499-sd_360_640_25fps.mp4",
+        like: 1334,
+        comments: 123,
+        save: 520,
+        shares: 73
+    }
+]
+
 </script>
--- src/components/mainContent.vue
+++ src/components/mainContent.vue
@@ -1,29 +1,22 @@
 <template>
     <section>
-        <div class="reels-container">
+        <div 
+        class="reels-container"
+        v-for="(video, index) in props.videoList"
+        :key="index"
+        >
             <div class="container">
                 <video 
-                    src="../assets/Videos/car1.mp4"
+                    :src="video.video"
                     class="car1-video"
                     autoplay
                     loop
                     muted
                 ></video>
-                <div class="video-description">
-                    <div >
-                        <p class="username"><strong>CarWow 8-21 </strong></p>
-                        <p class="description"><em>Old is gold!</em></p>
-                        <p class="hash-tags">#classic #oldMoney #cantAffordThis!</p>
-                        <p class="music-used">
-                            <img src="../assets/music.png" alt="music-logo">
-                            SCALE$-STAND UP GUY
-                        </p>
-                    </div>
-                </div>
                 <div class="interact-icons">
                     <div class="profile">
                         <img 
-                            src="../assets/profile3.jpeg"
+                            :src="video.profilePic"
                             alt="profile-picture"
                             class="profile-pic"
                         >
@@ -37,7 +30,7 @@
                                 class="like-logo"
                             >
                         </div>
-                        <p>932</p>
+                        <p>{{ video.like }}</p>
                     </div>
                     <div class="comment">
                         <div class="image">
@@ -47,7 +40,7 @@
                                 class="comments"
                             > 
                             </div>
-                        <p>234</p>
+                        <p>{{ video.comments }}</p>
                     </div>
                     <div class="save">
                         <div class="image">
@@ -57,7 +50,7 @@
                                 class="saves"
                             > 
                             </div> 
-                        <p>95</p>
+                        <p>{{ video.save }}</p>
                     </div>
                     <div class="share">
                         <div class="image">
@@ -67,364 +60,21 @@
                                 class="shares"
                             > 
                             </div> 
-                        <p>130</p>
+                        <p>{{ video.shares }}</p>
                     </div>
                 </div>
             </div> 
-            <div class="container">
-                <video 
-                    src="../assets/Videos/car1.mp4"
-                    class="car1-video"
-                    autoplay
-                    loop
-                    muted
-                ></video>
-                <div class="video-description">
-                    <div >
-                        <p class="username"><strong>CarWow 8-21 </strong></p>
-                        <p class="description"><em>Old is gold!</em></p>
-                        <p class="hash-tags">#classic #oldMoney #cantAffordThis!</p>
-                        <p class="music-used">
-                            <img src="../assets/music.png" alt="music-logo">
-                            SCALE$-STAND UP GUY
-                        </p>
-                    </div>
-                </div>
-                <div class="interact-icons">
-                    <div class="profile">
-                        <img 
-                            src="../assets/profile3.jpeg"
-                            alt="profile-picture"
-                            class="profile-pic"
-                        >
-                        <p><strong>+</strong></p>
-                    </div>
-                    <div class="like">
-                        <div class="image">
-                            <img 
-                                src="../assets/heart.png" 
-                                alt="like-logo"
-                                class="like-logo"
-                            >
-                        </div>
-                        <p>932</p>
-                    </div>
-                    <div class="comment">
-                        <div class="image">
-                            <img 
-                                src="../assets/chat-bubble.png" 
-                                alt="comment-logo"
-                                class="comments"
-                            > 
-                            </div>
-                        <p>234</p>
-                    </div>
-                    <div class="save">
-                        <div class="image">
-                            <img 
-                                src="../assets/bookmark.png" 
-                                alt="save-logo"
-                                class="saves"
-                            > 
-                            </div> 
-                        <p>95</p>
-                    </div>
-                    <div class="share">
-                        <div class="image">
-                            <img 
-                                src="../assets/share.png" 
-                                alt="share-logo"
-                                class="shares"
-                            > 
-                            </div> 
-                        <p>130</p>
-                    </div>
-                </div>
-            </div> 
-
-            <div class="container">
-                <video 
-                    src="../assets/Videos/car1.mp4"
-                    class="car1-video"
-                    autoplay
-                    loop
-                    muted
-                ></video>
-                <div class="video-description">
-                    <div >
-                        <p class="username"><strong>CarWow 8-21 </strong></p>
-                        <p class="description"><em>Old is gold!</em></p>
-                        <p class="hash-tags">#classic #oldMoney #cantAffordThis!</p>
-                        <p class="music-used">
-                            <img src="../assets/music.png" alt="music-logo">
-                            SCALE$-STAND UP GUY
-                        </p>
-                    </div>
-                </div>
-                <div class="interact-icons">
-                    <div class="profile">
-                        <img 
-                            src="../assets/profile3.jpeg"
-                            alt="profile-picture"
-                            class="profile-pic"
-                        >
-                        <p><strong>+</strong></p>
-                    </div>
-                    <div class="like">
-                        <div class="image">
-                            <img 
-                                src="../assets/heart.png" 
-                                alt="like-logo"
-                                class="like-logo"
-                            >
-                        </div>
-                        <p>932</p>
-                    </div>
-                    <div class="comment">
-                        <div class="image">
-                            <img 
-                                src="../assets/chat-bubble.png" 
-                                alt="comment-logo"
-                                class="comments"
-                            > 
-                            </div>
-                        <p>234</p>
-                    </div>
-                    <div class="save">
-                        <div class="image">
-                            <img 
-                                src="../assets/bookmark.png" 
-                                alt="save-logo"
-                                class="saves"
-                            > 
-                            </div> 
-                        <p>95</p>
-                    </div>
-                    <div class="share">
-                        <div class="image">
-                            <img 
-                                src="../assets/share.png" 
-                                alt="share-logo"
-                                class="shares"
-                            > 
-                            </div> 
-                        <p>130</p>
-                    </div>
-                </div>
-            </div> 
-            
-            <div class="container">
-                <video 
-                    src="../assets/Videos/car1.mp4"
-                    class="car1-video"
-                    autoplay
-                    loop
-                    muted
-                ></video>
-                <div class="video-description">
-                    <div >
-                        <p class="username"><strong>CarWow 8-21 </strong></p>
-                        <p class="description"><em>Old is gold!</em></p>
-                        <p class="hash-tags">#classic #oldMoney #cantAffordThis!</p>
-                        <p class="music-used">
-                            <img src="../assets/music.png" alt="music-logo">
-                            SCALE$-STAND UP GUY
-                        </p>
-                    </div>
-                </div>
-                <div class="interact-icons">
-                    <div class="profile">
-                        <img 
-                            src="../assets/profile3.jpeg"
-                            alt="profile-picture"
-                            class="profile-pic"
-                        >
-                        <p><strong>+</strong></p>
-                    </div>
-                    <div class="like">
-                        <div class="image">
-                            <img 
-                                src="../assets/heart.png" 
-                                alt="like-logo"
-                                class="like-logo"
-                            >
-                        </div>
-                        <p>932</p>
-                    </div>
-                    <div class="comment">
-                        <div class="image">
-                            <img 
-                                src="../assets/chat-bubble.png" 
-                                alt="comment-logo"
-                                class="comments"
-                            > 
-                            </div>
-                        <p>234</p>
-                    </div>
-                    <div class="save">
-                        <div class="image">
-                            <img 
-                                src="../assets/bookmark.png" 
-                                alt="save-logo"
-                                class="saves"
-                            > 
-                            </div> 
-                        <p>95</p>
-                    </div>
-                    <div class="share">
-                        <div class="image">
-                            <img 
-                                src="../assets/share.png" 
-                                alt="share-logo"
-                                class="shares"
-                            > 
-                            </div> 
-                        <p>130</p>
-                    </div>
-                </div>
-            </div> 
-
-            <div class="container">
-                <video 
-                    src="../assets/Videos/car1.mp4"
-                    class="car1-video"
-                    autoplay
-                    loop
-                    muted
-                ></video>
-                <div class="video-description">
-                    <div >
-                        <p class="username"><strong>CarWow 8-21 </strong></p>
-                        <p class="description"><em>Old is gold!</em></p>
-                        <p class="hash-tags">#classic #oldMoney #cantAffordThis!</p>
-                        <p class="music-used">
-                            <img src="../assets/music.png" alt="music-logo">
-                            SCALE$-STAND UP GUY
-                        </p>
-                    </div>
-                </div>
-                <div class="interact-icons">
-                    <div class="profile">
-                        <img 
-                            src="../assets/profile3.jpeg"
-                            alt="profile-picture"
-                            class="profile-pic"
-                        >
-                        <p><strong>+</strong></p>
-                    </div>
-                    <div class="like">
-                        <div class="image">
-                            <img 
-                                src="../assets/heart.png" 
-                                alt="like-logo"
-                                class="like-logo"
-                            >
-                        </div>
-                        <p>932</p>
-                    </div>
-                    <div class="comment">
-                        <div class="image">
-                            <img 
-                                src="../assets/chat-bubble.png" 
-                                alt="comment-logo"
-                                class="comments"
-                            > 
-                            </div>
-                        <p>234</p>
-                    </div>
-                    <div class="save">
-                        <div class="image">
-                            <img 
-                                src="../assets/bookmark.png" 
-                                alt="save-logo"
-                                class="saves"
-                            > 
-                            </div> 
-                        <p>95</p>
-                    </div>
-                    <div class="share">
-                        <div class="image">
-                            <img 
-                                src="../assets/share.png" 
-                                alt="share-logo"
-                                class="shares"
-                            > 
-                            </div> 
-                        <p>130</p>
-                    </div>
-                </div>
-            </div> 
-            
-            <div class="video2">
-                <video 
-                    src="../assets/Videos/car1.mp4"
-                    class="car1-video"
-                    autoplay
-                    loop
-                    muted
-                ></video>
-                <div class="video-description">
-                    <div >
-                        <p class="username"><strong>CarWow 8-21 </strong></p>
-                        <p class="description"><em>Old is gold!</em></p>
-                        <p class="hash-tags">#classic #oldMoney #cantAffordThis!</p>
-                        <p class="music-used">
-                            <img src="../assets/music.png" alt="music-logo">
-                            SCALE$-STAND UP GUY
-                        </p>
-                    </div>
-                </div>
-                <div class="interact-icons">
-                    <div class="profile">
-                        <img 
-                            src="../assets/profile3.jpeg"
-                            alt="profile-picture"
-                            class="profile-pic"
-                        >
-                        <p><strong>+</strong></p>
-                    </div>
-                    <div class="like">
-                        <div class="image">
-                            <img 
-                                src="../assets/heart.png" 
-                                alt="like-logo"
-                                class="like-logo"
-                            >
-                        </div>
-                        <p>932</p>
-                    </div>
-                    <div class="comment">
-                        <div class="image">
-                            <img 
-                                src="../assets/chat-bubble.png" 
-                                alt="comment-logo"
-                                class="comments"
-                            > 
-                            </div>
-                        <p>234</p>
-                    </div>
-                    <div class="save">
-                        <div class="image">
-                            <img 
-                                src="../assets/bookmark.png" 
-                                alt="save-logo"
-                                class="saves"
-                            > 
-                            </div> 
-                        <p>95</p>
-                    </div>
-                    <div class="share">
-                        <div class="image">
-                            <img 
-                                src="../assets/share.png" 
-                                alt="share-logo"
-                                class="shares"
-                            > 
-                            </div> 
-                        <p>130</p>
-                    </div>
-                </div>
-            </div>    
-        </div>
+        </div> 
     </section>
 </template>
+
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+    videoList:{
+        type: Array,
+        Required: true
+    }
+});
+</script>
